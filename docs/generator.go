bd97f0bfe1ce3e096c154e29dbcb7cd19186b101
Gordon Bleux <UiP9AV6Y+git@protonmail.com>
2020-06-14 15:02:22 +0200

    implement documentation generator.
    generate command help pages using `cobra/doc`

---
+++ docs/generator.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+
+	"github.com/UiP9AV6Y/changelog-assembler/pkg/cmd"
+)
+
+var Application = "changelog-assembler"
+var ManSection = "1"
+
+type DocGenerator struct {
+	Markdown   bool
+	Output     string
+	ManSection string
+
+	command *cobra.Command
+}
+
+func (g *DocGenerator) Command() *cobra.Command {
+	return g.command
+}
+
+func (g *DocGenerator) RunE(_ *cobra.Command, args []string) (err error) {
+	cmd := cmd.New(g.command.Use)
+	header := &doc.GenManHeader{
+		Title:   strings.ToUpper(g.command.Use),
+		Section: g.ManSection,
+	}
+
+	if err = os.MkdirAll(g.Output, 0755); err != nil {
+		return err
+	}
+
+	if g.Markdown {
+		err = doc.GenMarkdownTree(cmd.Command(), g.Output)
+	} else {
+		err = doc.GenManTree(cmd.Command(), header, g.Output)
+	}
+
+	return err
+}
+
+func NewDocGenerator(application, manSection string) cmd.Command {
+	output, _ := os.Getwd()
+	command := &DocGenerator{
+		Output:     output,
+		ManSection: manSection,
+	}
+	cmd := &cobra.Command{
+		Use:          application,
+		Short:        "Generate documentation",
+		Long:         "Create command documentation automatically",
+		RunE:         command.RunE,
+		Args:         cobra.NoArgs,
+		SilenceUsage: true,
+	}
+
+	cmd.Flags().StringVarP(&command.Output, "output", "o", command.Output, "Output directory")
+	cmd.Flags().BoolVarP(&command.Markdown, "markdown", "m", command.Markdown, "Generate markdown documentation instead of man pages")
+
+	command.command = cmd
+
+	return command
+}
+
+func main() {
+	gen := NewDocGenerator(Application, ManSection)
+
+	if err := gen.Command().Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golangci/golangci-lint v1.24.0
 	github.com/spf13/cobra v0.0.5
 	github.com/stretchr/testify v1.5.1
+	golang.org/x/tools v0.0.0-20200204192400-7124308813f3
 	gopkg.in/ini.v1 v1.51.0
-	gopkg.in/yaml.v2 v2.2.7
 	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c
 )
